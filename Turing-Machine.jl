4801500a94421da5e623a56f50f84794ca81b815
titiz <gtitiz@gmail.com>
2017-02-07 19:31:16 +0400

    arbitrary size; generalization of start condition

--- Turing-Machine.jl
+++ Turing-Machine.jl
@@ -10,15 +10,14 @@ type TuringMachine
     instructions::Array 
     current_state::Int8 
     strip::Array
+    break_limit::Int8
+    break_counter::Int8
     function TuringMachine()
-        new(3, [], 1, [])
+        new(1, [], 1, [], 100, 0)
     end
 end
 
 
-function set_pointer_location(tur_mach::TuringMachine, index::Int64)
-    tur_mach.pointer_location = index
-end
 
 
 function create_strip(tur_mach::TuringMachine, integer1, integer2)
@@ -41,8 +40,7 @@ function addInstruction(tur_mach::TuringMachine, instruction)
 end
 
 function readInstruction(tur_mach::TuringMachine, index::Int64)
-    instruction_done = false
-    println(tur_mach.pointer_location)
+    instruction_done = false 
     if tur_mach.current_state == tur_mach.instructions[index][1]
         if tur_mach.strip[tur_mach.pointer_location] == tur_mach.instructions[index][2]
             instruction_done = true
@@ -60,10 +58,40 @@ function readInstruction(tur_mach::TuringMachine, index::Int64)
     end
 end
 
+
+function resize(tur_mach::TuringMachine)
+    if tur_mach.pointer_location >= length(tur_mach.strip)
+        push!(tur_mach.strip, "-")
+        tur_mach.break_counter += 1
+    elseif tur_mach.pointer_location <= 0
+        tur_mach.break_counter += 1
+        tur_mach.strip = append!(tur_mach.array, ["-"])
+    end
+    
+    if tur_mach.break_counter > tur_mach.break_limit
+        return false
+    end
+    return true
+end
+
+function setPointerLocation(tur_mach::TuringMachine)
+    index = 1
+    while tur_mach.strip[index] != "x"
+        index += 1
+    end
+    tur_mach.pointer_location = index
+end
+
 function performTask(tur_mach::TuringMachine)
+    setPointerLocation(tur_mach)
     while tur_mach.current_state != 0
         for index = 1:length(tur_mach.instructions)
             readInstruction(tur_mach, index)
+            cont = resize(tur_mach)
+                if !cont 
+                    println("MAXIMUM ADDITION LIMIT EXCEEDED")
+                    return
+                end
         end
         println(tur_mach.current_state)
         println(tur_mach.strip)
@@ -84,6 +112,7 @@ printStrip(turing_machine)
 
 # Christine Dah-In Chung's Turing Machine for finding the center of a
 # an array of odd length
+
 
 addInstruction(turing_machine, [1, "x", "L", 2])
 addInstruction(turing_machine, [1, "-", "x", 0])
